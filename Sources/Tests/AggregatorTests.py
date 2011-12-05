e5b915a3aac14e3303d3ed190f91cbd6aad3c52b
Kor de Jong <kor@jemig.eu>
2011-12-05 17:30:35 +0100

    Busy with Aggregator and tests

--- Sources/Tests/AggregatorTests.py
+++ Sources/Tests/AggregatorTests.py
@@ -6,5 +6,15 @@ import TrackTime
 
 class AggregatorTests(unittest.TestCase):
 
-  pass
+  def test001(self):
+    """Test typical behavior"""
+    parser = TrackTime.Parser()
+    workedHours = parser.parse(file("WorkedHours-001.txt"))
+    sickHours = {}
+    vacationHours = {}
+    holidayHours = {}
+
+    aggregator = TrackTime.Aggregator(workedHours, sickHours, vacationHours,
+      holidayHours)
+
 
--- Sources/TrackTime/Aggregator.py
+++ Sources/TrackTime/Aggregator.py
@@ -3,6 +3,7 @@
 Aggregator
 **********
 """
+import datetime
 
 
 
@@ -13,9 +14,45 @@ class Aggregator(object):
   """
 
   def __init__(self,
-    records):
+    workedHours,
+    sickHours,
+    vacationHours,
+    holidayHours):
     """
     Create an Aggregator instance.
     """
-    self.records = records
+    self.workedHours = workedHours
+    self.sickHours = sickHours
+    self.vacationHours = vacationHours
+    self.holidayHours = holidayHours
+
+    # Aggregate per day.
+    self.hoursPerDay = {}
+    for collection in [self.workedHours, self.sickHours, self.vacationHours,
+      self.holidayHours]:
+      for date in collection:
+        year, week, day = date.isocalendar()
+        recordsPerDay = collection[date]
+
+        for record in recordsPerDay:
+          self.hoursPerDay[date] = self.hoursPerDay.get(date, 0) + \
+            record.nrHours
+
+    days = self.hoursPerDay.keys()
+    days.sort()
+
+    year = datetime.timedelta(days=365)
+    if (days[-1] - days[0]) > year:
+      raise ValueError("No support for records from more than one year, yet")
+
+    # Aggregate per week.
+    self.hoursPerWeek = {}
+    for week in range(1, 54):
+      self.hoursPerWeek[week] = 0.0
+
+    for date in self.hoursPerDay:
+      year, week, day = date.isocalendar()
+      self.hoursPerWeek[week] += self.hoursPerDay[date]
+      # self.lastWeekEntered = max(self.lastWeekEntered, week)
+
 
