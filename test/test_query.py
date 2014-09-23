126873580892fff261652560139fdfc99ef72211
Kor de Jong <kor@jemig.eu>
2014-09-23 09:21:45 +0200

    Added support for query hours

--- test/test_query.py
+++ test/test_query.py
@@ -8,20 +8,27 @@ import track_time
 
 class TestQuery(unittest.TestCase):
 
-    def test_grep_project(self):
+    def test_filter_projects_by_name(self):
         records = track_time.parse(file("sub_projects-001.txt"))
         self.assertEqual(len(records), 20)
 
-        selected_records = track_time.grep_projects(records)
+        selected_records = track_time.filter_projects_by_name(records)
         self.assertEqual(len(selected_records), 20)
 
-        selected_records = track_time.grep_projects(records,
+        selected_records = track_time.filter_projects_by_name(records,
             project_name_pattern="my_project_b/*")
         self.assertEqual(len(selected_records), 10)
 
-        selected_records = track_time.grep_projects(records,
+        selected_records = track_time.filter_projects_by_name(records,
             project_name_pattern="*/blog")
         self.assertEqual(len(selected_records), 4)
+
+    def test_filter_projects_by_date(self):
+        records = track_time.parse(file("sub_projects-001.txt"))
+        selected_records = track_time.filter_projects_by_date(records,
+            from_time_point=datetime.date(2014, 2, 2),
+            to_time_point=datetime.date(2014, 2, 5))
+        self.assertEqual(len(selected_records), 5)
 
     def test_merge_records_by_project(self):
         records = track_time.parse(file("sub_projects-001.txt"))
@@ -29,15 +36,22 @@ class TestQuery(unittest.TestCase):
         merged_records = track_time.merge_records_by_project(records)
         self.assertEqual(len(merged_records), 14)
 
+    def test_merge_records_by_date(self):
+        records = track_time.parse(file("sub_projects-001.txt"))
+
+        merged_records = track_time.merge_records_by_date(records)
+        self.assertEqual(len(merged_records), 16)
+
     def test_merge_records_by_category(self):
         records = track_time.parse(file("categories-001.txt"))
 
         merged_records = track_time.merge_records_by_category(records)
-        self.assertEqual(len(merged_records), 3)
+        self.assertEqual(len(merged_records), 4)
 
         self.assertEqual(merged_records[0].project, ["project"])
-        self.assertEqual(merged_records[1].project, ["vacation"])
-        self.assertEqual(merged_records[2].project, ["sick"])
+        self.assertEqual(merged_records[1].project, ["holiday"])
+        self.assertEqual(merged_records[2].project, ["vacation"])
+        self.assertEqual(merged_records[3].project, ["sick"])
 
     def test_merge_child_projects_with_parents(self):
         records = track_time.parse(file("sub_projects-001.txt"))
